9051aa07c134d7c24717e14b81a926093e119d17
CreatorDmitrij <codisculptor@gmail.com>
2025-12-08 22:04:01 +0100

    New path api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,1 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://norma.nomoreparties.space/api';
-
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://norma.education-services.ru/api';
